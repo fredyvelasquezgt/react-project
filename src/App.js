dd3da53d7c27a3920a6ddf6e22354fe426c81aca
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-22 11:09:54 -0600

    cleaning project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import Todo from './components/Todo';
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-     <Todo text='learn react'/>
-     <Todo text='master react' />
-     <Todo text='do laundry' />
-     
-
+    
     </div>
 
   );
